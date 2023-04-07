56dae88277a78d932f62f31cf78d07577b596eab
Tenzin Wangdhen <sinzin91@gmail.com>
2023-04-06 21:58:34 -0700

    more location permission fixing

--- src/components/LocationPermissionCheckbox.jsx
+++ src/components/LocationPermissionCheckbox.jsx
@@ -1,30 +1,32 @@
-import React, { useState } from 'react';
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import { styled } from '@mui/system';
+import React, { useState } from "react";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { styled } from "@mui/system";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const CustomFormControlLabel = styled(FormControlLabel)({
-  display: 'block',
-  marginBottom: '16px',
+  display: "block",
+  marginBottom: "16px",
 });
 
 const LocationPermissionCheckbox = (props) => {
   const [isChecked, setIsChecked] = useState(false);
-  const [countryCode, setCountryCode] = useState('');
+  const [countryCode, setCountryCode] = useState("");
 
   const handleCheckboxChange = async (e) => {
     setIsChecked(e.target.checked);
     if (e.target.checked) {
       await getCountryCode();
     } else {
-      setCountryCode('');
-      props.onCountryCodeChange('');
+      setCountryCode("");
+      props.onCountryCodeChange("");
     }
   };
 
   const getCountryCode = async () => {
     try {
-      const response = await fetch('https://ipapi.co/json/');
+      const response = await fetch("https://ipapi.co/json/");
       const data = await response.json();
       setCountryCode(data.country);
       props.onCountryCodeChange(data.country);
@@ -34,26 +36,49 @@ const LocationPermissionCheckbox = (props) => {
   };
 
   return (
-    <div>
-      <CustomFormControlLabel
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <FormControlLabel
         control={
           <Checkbox
             checked={isChecked}
             onChange={handleCheckboxChange}
             color="default"
             sx={{
-              color: 'white',
-              '&.Mui-checked': { color: 'primary.main' },
+              color: "white",
+              "&.Mui-checked": { color: "primary.main" },
             }}
           />
         }
         label={
-          isChecked
-            ? `Your country code is: ${countryCode}`
-            : "Allow access to your location, to get streaming services for your country."
+          isChecked ? (
+            <Typography
+              variant="body2"
+              sx={{
+                color: "white",
+              }}
+            >
+              Your country code is: {countryCode}
+            </Typography>
+          ) : (
+            <Typography
+              variant="body2"
+              sx={{
+                color: isChecked ? "white" : "rgba(255, 255, 255, 0.7)",
+              }}
+            >
+              Allow access to your location, to get streaming services for your country.
+            </Typography>
+          )
         }
       />
-    </div>
+    </Box>
   );
 };
 
